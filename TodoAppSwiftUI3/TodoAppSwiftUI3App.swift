53b7080f2e5fb98d56b1d8499ce426890a24435e
roman-luzgin <luzginn@gmail.com>
2021-06-21 22:39:22 +0200

    Initial Commit

---
+++ TodoAppSwiftUI3/TodoAppSwiftUI3App.swift
@@ -0,0 +1,20 @@
+//
+//  TodoAppSwiftUI3App.swift
+//  TodoAppSwiftUI3
+//
+//  Created by Roman Luzgin on 21.06.21.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppSwiftUI3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
